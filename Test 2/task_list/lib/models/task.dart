c5bf47d9a636457ee06c330e83a4d7157e4e2156
Jorge Antonio Martinez Tejeda <jamt1000@gmail.com>
2024-07-31 17:12:32 -0600

    wip(tasks)
    Add, show and delete tasks but a checkbok doesn't work when is checked, only when add or delete another task.

---
+++ Test 2/task_list/lib/models/task.dart
@@ -0,0 +1,17 @@
+import 'package:hive/hive.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String name;
+
+  @HiveField(2)
+  bool isCompleted;
+
+  Task({required this.id, required this.name, this.isCompleted = false});
+}
